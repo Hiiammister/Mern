20a2b028f658350595d5390a798897f030dc8dea
hiiammister <adityaborkar801@gmail.com>
2026-01-19 22:18:29 -0500

    Created routes and controller, refactored code

---
+++ backend/src/server.js
@@ -0,0 +1,19 @@
+import express from "express"
+import notesRoutes from "./routes/notesRoutes.js"
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+
+const app=express();
+const PORT=process.env.PORT || 5001
+connectDB();
+app.use("/api/notes/", notesRoutes);
+
+app.listen(PORT,()=> {
+    console.log("server started on port:", PORT);
+} );
+
+//dkdFp0kLBP1b8XHA
+//mongodb+srv://adityaanother1_db_user:dkdFp0kLBP1b8XHA@cluster0.lzbe8be.mongodb.net/?appName=Cluster0
